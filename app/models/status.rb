2cd7f277621d9296b8d7190866d3490ae2090c55
DennisIp <dennis.ip@utoronto.ca>
2013-11-01 12:34:29 -0400

    Update status.rb

--- app/models/status.rb
+++ app/models/status.rb
@@ -2,6 +2,8 @@ class Status < Field
     set_table_name "account status"
     attr_accessible :acct_status
 
+    # Returns the number of days the acct has been suspended if applicable, otherwise returns a message of elligiblity 
+    # to use the service (Banned or In Good Standings).
     def GetStatus()
 	if (self.acct_status < 0)
 	    return "Permanently Banned"
