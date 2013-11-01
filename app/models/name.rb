851005286b8826cecc6ee90ae5b8820eadaad1bd
DennisIp <dennis.ip@utoronto.ca>
2013-11-01 12:31:42 -0400

    Update name.rb

--- app/models/name.rb
+++ app/models/name.rb
@@ -2,6 +2,8 @@ class Name < Field
     set_table_name "name"
     attr_accessible :name
 
+    # When retrieving the name of the reseracher, must find a way to create a temporary anonymous name which can
+    # link two users after they have made contact with each other depending on the level of visibility selected
     def GetName()
 	if (self.hidden == true)    	
 		return 'John Doe'
